abc19f8d150539577fc3da3ad9ad6e47fe84c815
Srividhya <sriv@quickminutes.com>
2020-03-17 10:54:03 +0000

    Google Analytics code added to header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -23,6 +23,17 @@
 
         <link rel="stylesheet" href="{{url('css/app.css')}}">
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160803470-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-160803470-1');
+        </script>
+
+
     </head>
 
     <body>
