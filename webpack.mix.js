c4132241abd6f5af4d660939a0a4487aa08639d3
Srividhya <sriv@quickminutes.com>
2020-03-17 14:12:31 +0000

    webpack added styles.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    'resources/css/styles.css'
+], 'public/css/app.css');
