8827d1af5af19723449f0851ccb45474bd019176
Srividhya <sriv@quickminutes.com>
2020-03-17 14:01:51 +0000

    Email configuration

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Weblee\Mandrill\MandrillServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'MandrillMail'  => Weblee\Mandrill\MandrillFacade::class
     ],
 
 ];
