f34c281f77f460e086034de97f832d9668bc05e6
Martin Bullman <cryptoknight1988@gmail.com>
2020-03-17 11:17:24 +0000

    Updated the favicon logo to new SVG

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,7 +5,7 @@
     <head>
 
         <!-- favicon-->
-        <link rel="shortcut icon" href="{{ url('img/logo.svg') }}" />
+        <link rel="shortcut icon" href="{{ url('img/favicon.svg') }}" />
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
