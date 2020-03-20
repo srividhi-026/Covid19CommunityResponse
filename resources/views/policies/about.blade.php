8bbba2b1f6ddbf44faac747f821c00bd8e22b7f4
Srividhya <sriv@quickminutes.com>
2020-03-20 16:14:39 +0000

    About blade updated

--- resources/views/policies/about.blade.php
+++ resources/views/policies/about.blade.php
@@ -1,28 +1,92 @@
 @include('layout.header')
-<h3 class="text-center">About us</h3>
-<div class="container">
-<p class="para"><b>
-Covid Community Response aims to bring communities together, collaborating and supporting those most in need during the Covid-19 outbreak in Ireland.
-</b></p>
-<p class="para">
-The Covid Community Response team was set up in an effort to help coordinate ongoing national & local community initiatives in the fight against Covid-19, standardising communication and campaigns, as well as sharing resources.
-</p>
-<p class="para">
-Our project is an open source build which will give others in the community the opportunity to continue to grow our build, and integrate projects they are currently working on. The key to this project is that we get every group working through one system.
-</p>
-<h5><b>
-Campaigns</b></h5>
-<p class="para">
-In these uncertain times it is fantastic to see so many members of our society pull together and launch various campaigns to tackle this challenge head on. The energy, creativity and passion which is on display is most certainly our silver lining. There are many campaigns currently underway with the most prevalent, overarching theme being to help people who are in isolation.
-</p>
-<p class="para">
 
-Standardising campaigns will allow community leaders to ‘rinse & repeat’ campaigns and campaign materials. CCR’s website will give people an opportunity to register as volunteers, giving access to support from a network of individuals working together within the communities. CCR’s fundraiser will see all proceeds going towards direct costs, including food, sanitary products, and supplies needed by those most affected by Covid-19.
-</p>
-<p class="para">
-CCR are working closely with organisations who are currently operating in the community but, due to extreme pressures, need more support at this time.
-</p>
-</div>
+    <h4 class="text-center">Covid Community Response</h4>
+
+        <div class="container">
+
+            <p class="para">
+                Covid Community Response is a national support helpline for individuals and organisations seeking
+                assistance from their local community during the Covid-19 pandemic. Members of the public seeking
+                assistance can now do so via text <b>(086 180 0256)</b> or phone call <b>(021 237 7809)</b>.
+
+            </p>
+
+            <br><br>
+
+            <div class="text-center">
+
+                <img src="{{url('/img/helpline_photo.png')}}" alt="Helpline numbers photo" />
+
+            </div>
+
+            <br>
+
+            <h6><b>Who We Are</b></h6>
+
+            <p class="para">
+                Covid Community Response is entirely staffed by volunteers who, seeing the needs of their community,
+                came together to make sure nobody is without support. Have a look at some of our team members here.
+            </p>
+
+            <h6><b>What Support Can I Receive?</b></h6>
+
+            <p class="para">
+                Our volunteers are ready to assist wherever they are needed. Whether it’s picking up groceries
+                or ensuring you have your prescriptions, our volunteers are there for you. For organisations
+                seeking extra support at this, we’d be delighted to assist with our national network of volunteers.
+            </p>
+
+            <br>
+
+            <div class="text-center">
+
+                <img src="{{url('/img/operate_method.png')}}" alt="How CCR operates"/>
+
+            </div>
+
+            <br>
+
+            <h6><b>How Can I Help?</b></h6>
+
+            <p class="para">
+                You can volunteer or <a href="https://www.ifundraise.ie/5173_covid-community-response.html" target="_blank">donate</a>.
+                Our volunteers work in a variety of roles. Many are out in their local communities offering
+                assistance to their neighbours, or a community organisation. We also have
+                people assisting on our national helpline, and coordinating our national network of volunteers.
+                If you’d like to help, please register <a href="https://covidcommunityresponse.ie/register">here</a>,
+                and a member of our team will be in touch with you.
+            </p>
+
+            <h6><b>I Have To Self-Isolate. I’d Also Like To Help.</b></h6>
+
+            <p class="para">
+                Welcome on board! We have many roles that we need support with. If you need to self-isolate,
+                or can’t leave home at all at this time, there is much you can help with. You can register
+                <a href="https://covidcommunityresponse.ie/register">here</a>.
+            </p>
+
+            <h6><b>What Happens When I Register?</b></h6>
+
+            <p class="para">
+                To volunteer, you can sign up <a href="https://covidcommunityresponse.ie/register">here</a>.
+                Once you’ve done that, our team will be in touch should anyone require assistance in your area.
+                We look forward to having you on board!
+            </p>
+
+            <h6><b>I Have Questions About Registering</b></h6>
+
+            <p class="para">
+                We’d love to answer them! Please contact us <a href="mailto:info@covidcommunityresponse.ie">here</a>.
+            </p>
+
+            <h6><b>I’m a Member Of The Press Looking To Contact Covid Community Response</b></h6>
+
+            <p class="para">
+                Please contact our communications team <a href="mailto:press@covidcommunityresponse.ie">here</a>.
+            </p>
+            <br>
+
+        </div>
 
    <!-- compiled and minified JavaScript
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
