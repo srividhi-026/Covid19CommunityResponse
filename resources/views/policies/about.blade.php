d45767b5579feccef3ac352215db34f40bea8f32
Srividhya <sriv@quickminutes.com>
2020-03-20 16:50:05 +0000

    minor change

--- resources/views/policies/about.blade.php
+++ resources/views/policies/about.blade.php
@@ -1,98 +1,91 @@
 @include('layout.header')
 
-    <h4 class="text-center">Covid Community Response</h4>
+    <h4 class="text-center">About Us</h4>
 
-        <div class="container">
+    <div class="container">
 
-            <p class="para">
-                Covid Community Response is a national support helpline for individuals and organisations seeking
-                assistance from their local community during the Covid-19 pandemic. Members of the public seeking
-                assistance can now do so via text <b>(086 180 0256)</b> or phone call <b>(021 237 7809)</b>.
+        <p class="para">
+            Covid Community Response is a national support helpline for individuals and organisations seeking
+            assistance from their local community during the Covid-19 pandemic. Members of the public seeking
+            assistance can now do so via text <b>(086 180 0256)</b> or phone call <b>(021 237 7809)</b>.
 
-            </p>
+        </p>
 
-            <br><br>
+        <br><br>
 
-            <div class="text-center">
+        <div class="text-center">
 
-                <img src="{{url('/img/helpline_photo.png')}}" alt="Helpline numbers photo" />
-
-            </div>
-
-            <br>
-
-            <h6><b>Who We Are</b></h6>
-
-            <p class="para">
-                Covid Community Response is entirely staffed by volunteers who, seeing the needs of their community,
-                came together to make sure nobody is without support. Have a look at some of our team members here.
-            </p>
-
-            <h6><b>What Support Can I Receive?</b></h6>
-
-            <p class="para">
-                Our volunteers are ready to assist wherever they are needed. Whether it’s picking up groceries
-                or ensuring you have your prescriptions, our volunteers are there for you. For organisations
-                seeking extra support at this, we’d be delighted to assist with our national network of volunteers.
-            </p>
-
-            <br>
-
-            <div class="text-center">
-
-                <img src="{{url('/img/operate_method.png')}}" alt="How CCR operates"/>
-
-            </div>
-
-            <br>
-
-            <h6><b>How Can I Help?</b></h6>
-
-            <p class="para">
-                You can volunteer or <a href="https://www.ifundraise.ie/5173_covid-community-response.html" target="_blank">donate</a>.
-                Our volunteers work in a variety of roles. Many are out in their local communities offering
-                assistance to their neighbours, or a community organisation. We also have
-                people assisting on our national helpline, and coordinating our national network of volunteers.
-                If you’d like to help, please register <a href="https://covidcommunityresponse.ie/register">here</a>,
-                and a member of our team will be in touch with you.
-            </p>
-
-            <h6><b>I Have To Self-Isolate. I’d Also Like To Help.</b></h6>
-
-            <p class="para">
-                Welcome on board! We have many roles that we need support with. If you need to self-isolate,
-                or can’t leave home at all at this time, there is much you can help with. You can register
-                <a href="https://covidcommunityresponse.ie/register">here</a>.
-            </p>
-
-            <h6><b>What Happens When I Register?</b></h6>
-
-            <p class="para">
-                To volunteer, you can sign up <a href="https://covidcommunityresponse.ie/register">here</a>.
-                Once you’ve done that, our team will be in touch should anyone require assistance in your area.
-                We look forward to having you on board!
-            </p>
-
-            <h6><b>I Have Questions About Registering</b></h6>
-
-            <p class="para">
-                We’d love to answer them! Please contact us <a href="mailto:info@covidcommunityresponse.ie">here</a>.
-            </p>
-
-            <h6><b>I’m a Member Of The Press Looking To Contact Covid Community Response</b></h6>
-
-            <p class="para">
-                Please contact our communications team <a href="mailto:press@covidcommunityresponse.ie">here</a>.
-            </p>
-            <br>
+            <img src="{{url('/img/helpline_photo.png')}}" alt="Helpline numbers photo" />
 
         </div>
 
-   <!-- compiled and minified JavaScript
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <br>
 
-<!-- font awesome icons -->
-<!-- <script defer src="https://kit.fontawesome.com/c94020147f.js" crossorigin="anonymous"></script>
-<script defer src="{{ url('js/app.js') }}"></script> -->
+        <h6><b>Who We Are</b></h6>
+
+        <p class="para">
+            Covid Community Response is entirely staffed by volunteers who, seeing the needs of their community,
+            came together to make sure nobody is without support. Have a look at some of our team members here.
+        </p>
+
+        <h6><b>What Support Can I Receive?</b></h6>
+
+        <p class="para">
+            Our volunteers are ready to assist wherever they are needed. Whether it’s picking up groceries
+            or ensuring you have your prescriptions, our volunteers are there for you. For organisations
+            seeking extra support at this, we’d be delighted to assist with our national network of volunteers.
+        </p>
+
+        <br>
+
+        <div class="text-center">
+
+            <img src="{{url('/img/operate_method.png')}}" alt="How CCR operates"/>
+
+        </div>
+
+        <br>
+
+        <h6><b>How Can I Help?</b></h6>
+
+        <p class="para">
+            You can volunteer or <a href="https://www.ifundraise.ie/5173_covid-community-response.html" target="_blank">donate</a>.
+            Our volunteers work in a variety of roles. Many are out in their local communities offering
+            assistance to their neighbours, or a community organisation. We also have
+            people assisting on our national helpline, and coordinating our national network of volunteers.
+            If you’d like to help, please register <a href="https://covidcommunityresponse.ie/register">here</a>,
+            and a member of our team will be in touch with you.
+        </p>
+
+        <h6><b>I Have To Self-Isolate. I’d Also Like To Help.</b></h6>
+
+        <p class="para">
+            Welcome on board! We have many roles that we need support with. If you need to self-isolate,
+            or can’t leave home at all at this time, there is much you can help with. You can register
+            <a href="https://covidcommunityresponse.ie/register">here</a>.
+        </p>
+
+        <h6><b>What Happens When I Register?</b></h6>
+
+        <p class="para">
+            To volunteer, you can sign up <a href="https://covidcommunityresponse.ie/register">here</a>.
+            Once you’ve done that, our team will be in touch should anyone require assistance in your area.
+            We look forward to having you on board!
+        </p>
+
+        <h6><b>I Have Questions About Registering</b></h6>
+
+        <p class="para">
+            We’d love to answer them! Please contact us <a href="mailto:info@covidcommunityresponse.ie">here</a>.
+        </p>
+
+        <h6><b>I’m a Member Of The Press Looking To Contact Covid Community Response</b></h6>
+
+        <p class="para">
+            Please contact our communications team <a href="mailto:press@covidcommunityresponse.ie">here</a>.
+        </p>
+        <br>
+
+    </div>
 
 @include('layout.footer')
