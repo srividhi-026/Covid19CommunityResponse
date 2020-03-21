29756d473410e0f91f96a784c1dc9b5618a70ac7
Srividhya <sriv@quickminutes.com>
2020-03-21 11:22:02 +0000

    Team members with images

--- resources/views/policies/team.blade.php
+++ resources/views/policies/team.blade.php
@@ -1,16 +1,20 @@
 @include('layout.header')
 
-    <h3 class="text-center">Our team</h3>
+    <h3 class="text-center">Team</h3>
 
     <div class="container">
 
         <h5><b>Danny O’Donovan</b></h5>
 
-        <p>Danny O’ Donovan has a history of public service, a former Governor of Cork Institute of Technology he
+        <img src="{{url('/img/danny.png')}}" alt="Danny O’Donovan"></img>
+
+        <p><a href="https://www.echolive.ie/opinion/Cork-entrepreneur-Dream-big-and-keep-your-feet-on-the-ground-4cc1ed44-823d-4dc6-83ec-d6fdb479a2bf-ds" style="text-decoration:none;" target="_blank">Danny O’ Donovan</a> has a history of public service, a former Governor of Cork Institute of Technology he
         is now the current CEO of QuickMinutes.com.  He enjoys process engineering, problem solving and even more so
         when it can have a positive impact on society.</p>
 
         <h5><b>Dave Graham</b></h5>
+
+        <img src="{{url('/img/dave.jpg')}}" style="width:197px;" alt="Dave Graham"></img>
 
         <p>Dave Graham is a Director of Emerging Technologies and Thought Leadership at Dell Technologies.
         He is a former social worker and therapist and loves working on the intersection of social responsibility
@@ -18,11 +22,15 @@
 
         <h5><b>Séamus Ó Fátharta</b></h5>
 
+        <img src="{{url('/img/se.jpeg')}}" style="width:197px;" alt="Séamus Ó Fátharta"></img>
+
         <p>Séamus is CEO of Óvis Media, a marketing and development company, and project manager of technology in
         education programme, TechSpace, through the Irish Language with Camara Ireland. He has worked with many NGOs
         and charities, including Claddagh Watch, of which he is a founder.</p>
 
         <h5><b>Jules Ní Chonchobhair</b></h5>
+
+        <img src="{{url('/img/ju.png')}}" style="width:197px;" alt="Jules Ní Chonchobhair"></img>
 
         <p>Jules has a background in engineering, technology and communications. She currently trains educators in areas
         of STEAM trí Ghaeilge on the TechSpace programme with Camara Ireland, including computer science, robotics,
@@ -33,16 +41,22 @@
 
         <h5><b>Michaela McDonald</b></h5>
 
+        <img src="{{url('/img/mi.jpg')}}" style="width:197px;" alt="Michaela McDonald"></img>
+
         <p>Michaela is a second year medical student. She has also studied languages, geography and performing arts.
         She works with Óvis Media as an advertising and marketing executive.</p>
 
         <h5><b>Conor Hackett</b></h5>
+
+        <img src="{{url('/img/conor.jpg')}}" style="width:197px;" alt="Conor Hackett"></img>
 
         <p>Conor is a Software Engineer with a background in FinTech and Payments. He has been helping out with
         technical solutions for the platform to get up and running quickly. Conor has also been keeping an eye on
         optimising for scale in the event of a large influx of requests and growing number of team members.</p>
 
         <h5><b>Michael Linehan</b></h5>
+
+        <img src="{{url('/img/mixer.png')}}" style="width:197px;" alt="Michael Linehan"></img>
 
         <p>Michael is a freelance consultant, currently heading up operations for the Industrial Internet. He also owns
         his own online clothing company Signature Style, which combines performance activewear with smart, casual office
